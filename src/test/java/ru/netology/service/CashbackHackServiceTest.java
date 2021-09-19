1fc69d0bf0d880b4bd2a2ba7100ba8f78b5f2250
Korotkov Dmitry <2110821@mail.ru>
2021-09-19 16:42:06 +0300

    junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldTestRemainIfAmountIs900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldTestRemainIfAmountIs999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldTestRemainIfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldTestRemainIfAmountIs1001() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected,actual);
+    }
+
+}
